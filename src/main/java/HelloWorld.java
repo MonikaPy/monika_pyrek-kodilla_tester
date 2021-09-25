40def783b054147fc91116f04d854693bc2b7305
Monika Pyrek <monikawrona15@gmail.com>
2021-09-25 14:25:33 +0200

    My changes

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!") ;
+        System.out.println("Monika") ;
+        System.out.println(31) ;
+        System.out.println('P') ;
+    }
+}
