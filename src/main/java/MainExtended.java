9efab8486a91f9ada7993317269b16f915eb9d6b
Monika Pyrek <monikawrona15@gmail.com>
2021-09-25 14:58:05 +0200

    My changes

---
+++ src/main/java/MainExtended.java
@@ -0,0 +1,16 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String greeting = "Hello!";
+        int price = 145;
+        char foreignExchange = '$';
+        double meters = 5.98;
+        boolean correctness = false;
+        String exit = "Goodbye!";
+        System.out.println(greeting);
+        System.out.println(price);
+        System.out.println(foreignExchange);
+        System.out.println(meters);
+        System.out.println(correctness);
+        System.out.println(exit);
+    }
+}
---
+++ src/main/java/proba.java
@@ -0,0 +1,6 @@
+public class proba {
+    public static void main(String[] args) {
+        String greeting = "Hello from FirstClass!";
+        System.out.println(greeting);
+    }
+}
