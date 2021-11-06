42f90ec9b2fb9afb763c1f42c05bcb7ad9868445
Monika Pyrek <monikawrona15@gmail.com>
2021-11-06 15:32:58 +0100

    Task

---
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -0,0 +1,9 @@
+package com.kodilla.collections.adv.maps.homework;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class School {
+    private List<Integer> school = new ArrayList<>();
+    private String name;
+}
