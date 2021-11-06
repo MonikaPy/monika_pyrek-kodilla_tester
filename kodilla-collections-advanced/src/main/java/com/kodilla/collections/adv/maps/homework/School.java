4ad180893a1ceed2058098f0e369a4a89d6d3895
Monika Pyrek <monikawrona15@gmail.com>
2021-11-06 17:46:09 +0100

    5.2

--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/School.java
@@ -4,6 +4,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class School {
-    private List<Integer> school = new ArrayList<>();
     private String name;
+    private List<Integer> school = new ArrayList<>();
+
+
+    public School(String name, int... school) {
+        this.name = name;
+        for (int student : school)
+            this.school.add(student);
+    }
+
+    public String getName() {
+        return name;
+    }
+    public int getSum() {
+        double sum = 0;
+        for (int student : school)
+            sum += student;
+        return (int) sum; //czemu tu int w () podpowiada
+    }
+
+    @Override
+    public String toString() {
+        return "Liczba uczniów w szkole: "+ school.toString();
+    }
 }
--- kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
+++ kodilla-collections-advanced/src/main/java/com/kodilla/collections/adv/maps/homework/SchoolDirectory.java
@@ -1,7 +1,31 @@
 package com.kodilla.collections.adv.maps.homework;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class SchoolDirectory {
     public static void main(String[] args) {
+        Map<Principal, School> schoolPricipal = new HashMap<>();
 
+        Principal Anna = new Principal("Anna Kowalska");
+        Principal Jan = new Principal("Jan Nowak");
+        Principal Izabella = new Principal("Izabella Skowron");
+        Principal Tomasz = new Principal("Tomasz Rataj");
+
+        School podstawowka = new School("Szkoła podstawowa nr 5", 1, 2, 4, 8, 9, 14);
+        School gimnazjum = new School("Gimnazjum", 14, 7, 22, 5, 8, 11, 19);
+        School liceum = new School("Liceum nr 3", 3, 17, 25);
+        School technikum = new School("Technikum nr 15", 10, 16, 4, 8, 27);
+
+        schoolPricipal.put(Anna, podstawowka);
+        schoolPricipal.put(Jan, gimnazjum);
+        schoolPricipal.put(Izabella, liceum);
+        schoolPricipal.put(Tomasz, technikum);
+
+        for (Map.Entry<Principal, School> principalSchoolEntry : schoolPricipal.entrySet())
+            System.out.println(principalSchoolEntry.getKey().getFirstname() + ", jest dyrektorem  " +
+                    principalSchoolEntry.getValue().getName() +", szkoła ta ma " + principalSchoolEntry.getValue().getSum()+" uczniów" );
     }
+
+
 }
