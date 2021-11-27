d8af7845b0c7a5f46e251e647bc7eef4e0ddc611
Monika Pyrek <monikawrona15@gmail.com>
2021-11-27 16:51:12 +0100

    7

--- kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/execution_model/homework/Shop.java
@@ -1,4 +1,36 @@
 package com.kodilla.execution_model.homework;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Shop {
+    private List<Order> orders = new ArrayList<>();
+
+    public void addOrder(Order order) {
+        this.orders.add(order);
+    }
+
+    public List<Order> getOrder(LocalDate beginning, LocalDate end) {
+        return orders.stream().filter(n->n.getOrderDate().isBefore(end))
+                .filter(n->n.getOrderDate().isAfter(beginning))
+                .collect(Collectors.toList());
+    }
+
+    public List<Order> valueRange(int minValue, int maxValue) {
+        return orders.stream().filter(n->n.getOrderValue()<maxValue)
+                .filter(n->n.getOrderValue()>minValue)
+                .collect(Collectors.toList());
+    }
+
+    public int getSize() {
+        return this.orders.size();
+    }
+
+    public int valueSum(){
+        return orders.stream().map(n->n.getOrderValue())
+                .mapToInt(n->n)
+                .sum();
+    }
 }
--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/PersonTestSuite.java
@@ -1,4 +1,19 @@
 package com.kodilla.parametrized_tests.homework;
 
-public class PersonTestSuite {
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonTestSuite {
+
+
+    @ParameterizedTest
+    @MethodSource(value = "com.kodilla.parametrized_tests.homework.PersonSource#providePersonForTestingBmi")
+    public void shouldCalculateBmi(String input, double height, double weight) {
+        Person person = new Person(height, weight);
+        assertEquals(input, person.getBMI());
+
+    }
 }
