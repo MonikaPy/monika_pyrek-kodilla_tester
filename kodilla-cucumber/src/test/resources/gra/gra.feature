92836d3b9cbbc9ee1f7e80673f239b8a18e8fbb5
Monika Pyrek <monikawrona15@gmail.com>
2022-01-15 18:34:37 +0100

    14.2

---
+++ kodilla-cucumber/src/test/resources/gra/gra.feature
@@ -0,0 +1,11 @@
+Feature: gra
+
+  Scenario Outline: Przez ile dzieli się liczba
+    Given Czy liczba dzieli się przez  <liczba>
+    When Pytam czy dana liczba dzieli się
+    Then Powinienen powiedzieć <odpowiedz>
+    Examples:
+      | liczba | odpowiedz|
+    | 3 | "Fizz"   |
+    | 5 | "Buzz" |
+    | 3 && 5 | "FizzBuzz" |
--- kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCss.java
+++ kodilla-selenium/src/main/java/com/kodilla/selenium/allegro/AllegroTestingAppCss.java
@@ -1,18 +1,32 @@
-//package com.kodilla.selenium.allegro;
-//
-//import org.openqa.selenium.Alert;
-//import org.openqa.selenium.By;
-//import org.openqa.selenium.WebDriver;
-//import org.openqa.selenium.WebElement;
-//import org.openqa.selenium.chrome.ChromeDriver;
-//import org.openqa.selenium.support.ui.ExpectedConditions;
-//import org.openqa.selenium.support.ui.Select;
-//import org.openqa.selenium.support.ui.WebDriverWait;
-//
-//import java.util.List;
-//
-//public class AllegroTestingAppCss {
-//    public static void main(String[] args) {
-//        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
-//        WebDriver driver = new ChromeDriver();
-//        driver.get("https://www.allegro.pl");
+package com.kodilla.selenium.allegro;
+
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
+
+public class AllegroTestingAppCss {
+    public static void main(String[] args) throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "c:\\selenium-drivers\\chrome\\chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://www.ebay.com");
+        WebElement categoryCombo = driver.findElement(
+                By.cssSelector("#gh-cat"));
+
+        Select yearSelect = new Select(categoryCombo);
+        yearSelect = new Select(categoryCombo);
+        yearSelect.selectByIndex(12);
+        WebElement inputField = driver.findElement(By.cssSelector("#gh-ac"));
+        inputField.sendKeys("Mavic mini");
+        inputField.submit();
+//        Thread.sleep(3000);
+//        List<WebElement> elements=driver.findElements(By.cssSelector("s-item_wrapper"));
+//        System.out.println(elements.size());
+    }
+}
